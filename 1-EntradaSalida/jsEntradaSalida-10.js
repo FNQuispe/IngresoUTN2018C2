3c77bc429e200f956cc2f914fd71f7786a65cacf
Fernando Nicolás Quispe <40809079+FNQuispe@users.noreply.github.com>
2018-07-04 19:35:52 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -2,7 +2,19 @@
 Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
-function mostrarAumento()
+function mostrarImporteConDescuento()
 {
-	
+	var importe = parseInt(document.getElementById("importe").value);
+
+    var importeConDescuento = importe - calcularDescuento(importe, 25);
+
+    document.getElementById("resultado").value = importeConDescuento;
+}
+
+/*
+retorna el resultado del porcentaje a aplicar en el importe
+*/ 
+function calcularDescuento(importe, porcentajeDescuento) 
+{
+    return (importe * porcentajeDescuento) / 100;
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,10 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+    var importeSueldo = parseInt(document.getElementById("sueldo").value);
+    var aumento = (importeSueldo * 10) / 100;
+    var importeConAumento = importeSueldo + aumento;
 
+    document.getElementById("resultado").value = importeConAumento;
 	
 }
