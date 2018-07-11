b11ac3b535abbf6386313688b5f2adade5569a04
Fernando Nicolás Quispe <40809079+FNQuispe@users.noreply.github.com>
2018-07-10 22:15:25 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,8 @@
 
 function mostrar()
 {
-
+    var ancho = parseInt(prompt("Ingrese el ancho de su rectangulo"));
+    var largo = parseInt(prompt("Ahora el largo"));
+    var perimetro = 2 * (ancho + largo);
+    alert("El perimetro de su rectangulo es: " + perimetro);
 }
